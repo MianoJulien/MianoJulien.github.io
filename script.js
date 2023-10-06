6a5c4dabe8686fbe103981d44a3c243fe70e1425
Julien <mianojulien57@gmail.com>
2023-10-06 13:35:49 +0200

    modification de la navbar et ajout des liens

---
+++ script.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector(".menu-trigger");
+
+burger.each(function (index) {
+  var $this = $(this);
+
+  $this.on("click", function (e) {
+    e.preventDefault();
+    $(this).toggleClass("active-" + (index + 1));
+  });
+});
